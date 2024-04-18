0b42a7bc6153e2de067d17f2a3a000f9d0c93fb8
https://bitnet-io.org <46433702+c4pt000@users.noreply.github.com>
2024-04-18 12:52:38 -0700

    Update fedora39-post-install.sh

--- fedora39-post-install.sh
+++ fedora39-post-install.sh
@@ -15,7 +15,8 @@ sh ip-reset.sh
 
 
 
-
+wget https://raw.githubusercontent.com/c4pt000/mega-blocker-iptables-ipset-04-17-2024/main/ipset-apply.sh
+chmod +x ./ipset-apply.sh
 
 
 # this will block all tor-traffic "DO NOT USE IF YOU WANT TO USE THE TOR-BROWSER OR TORD OR TORRIFY ON YOUR HOST
